1760c354d433995047a19ac2b3751f0b764342ad
Shashank Atmakur <ashashank1@outlook.com>
2025-10-25 01:27:50 +0530

    Add project files.

---
+++ Pages/Forgot.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SMS.Pages
+{
+    public class ForgotModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            // placeholder: send code logic can be implemented here
+        }
+    }
+}
---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SMS.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            // Placeholder: authentication logic can be added here later.
+        }
+    }
+}
